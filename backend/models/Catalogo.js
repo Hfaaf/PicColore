a8ebe715e4fbaca932549f19a5731ee189af9bb4
Hfaaf <heitorfariaspinheirodoamaral@gmail.com>
2025-06-02 15:49:57 -0300

    MODELATT

--- backend/models/Catalogo.js
+++ backend/models/Catalogo.js
@@ -4,7 +4,11 @@ const catalogoSchema = new mongoose.Schema({
     nome: String,
     imagem: String,
     descricao: String,
-    order: { type: Number, default: 0 }
+    order: { 
+        type: Number, 
+        default: 0,
+        required: true 
+    }
 });
 
 export default mongoose.model('Catalogo', catalogoSchema);
