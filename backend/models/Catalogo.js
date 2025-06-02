2c7e62125390c1a4578536001d0516ece0266487
Hfaaf <heitorfariaspinheirodoamaral@gmail.com>
2025-06-02 15:22:06 -0300

    RETURNBACK

--- backend/models/Catalogo.js
+++ backend/models/Catalogo.js
@@ -1,10 +1,8 @@
 import mongoose from 'mongoose';
-
 const catalogoSchema = new mongoose.Schema({
     nome: String,
     imagem: String,
     descricao: String,
-    order: { type: Number, default: 0 }
 });
 
 export default mongoose.model('Catalogo', catalogoSchema);
--- backend/routes/catalogoRoutes.js
+++ backend/routes/catalogoRoutes.js
@@ -1,13 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import auth from '../middleware/authMiddleware.js';
-import { 
-    listCatalogo, 
-    addCatalogo, 
-    deleteCatalogo, 
-    updateCatalogo,
-    updateOrder 
-} from '../controllers/catalogoController.js';
+import { listCatalogo, addCatalogo, deleteCatalogo, updateCatalogo } from '../controllers/catalogoController.js';
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
@@ -16,6 +10,5 @@ router.get('/', listCatalogo);
 router.post('/', auth, upload.single('imagem'), addCatalogo);
 router.delete('/:id', auth, deleteCatalogo);
 router.put('/:id', auth, upload.single('imagem'), updateCatalogo);
-router.put('/order', auth, updateOrder);
 
 export default router;
