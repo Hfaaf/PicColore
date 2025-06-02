f04e2f1f32ace8efea53bb1a3b38e843831d44ce
Hfaaf <heitorfariaspinheirodoamaral@gmail.com>
2025-06-02 15:28:46 -0300

    Test

--- backend/models/Catalogo.js
+++ backend/models/Catalogo.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+
 const catalogoSchema = new mongoose.Schema({
     nome: String,
     imagem: String,
     descricao: String,
+    order: { type: Number, default: 0 }
 });
 
 export default mongoose.model('Catalogo', catalogoSchema);
